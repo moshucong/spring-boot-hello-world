4147eb06d945a11ff290a37237dc136c9f3df103
莫树聪 <allen.mo@yeahmobi.com>
2017-10-24 10:34:30 +0800

    1

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,6 +1,9 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -8,6 +11,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
+    	System.out.println( "log test : "  + (new Date()));
         return "The app is running! ";
     }
     
